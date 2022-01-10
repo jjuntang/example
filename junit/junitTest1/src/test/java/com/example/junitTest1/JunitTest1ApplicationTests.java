96af976f732764aa737b1fc3a97e83f03fa19171
pjo <jjuntang@naver.com>
2022-01-10 23:58:55 +0900

    2022.01.10, junit

---
+++ junit/junitTest1/src/test/java/com/example/junitTest1/JunitTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junitTest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
