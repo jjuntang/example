46684493a4fa68f833fdbc0e34f1426bd141e4de
es.lee@chargev.co.kr <es.lee@chargev.co.kr>
2022-01-11 15:28:53 +0900

    2022.01.11 - json

---
+++ java/json01/src/main/java/com/example/json/Car.java
@@ -0,0 +1,34 @@
+package com.example.json;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor
+public class Car {
+
+    private String color;
+    private String type;
+
+//    public Car(String color, String type) {
+//        this.color = color;
+//        this.type = type;
+//    }
+//
+//    public String getColor() {
+//        return color;
+//    }
+//
+//    public void setColor(String color) {
+//        this.color = color;
+//    }
+//
+//    public String getType() {
+//        return type;
+//    }
+//
+//    public void setType(String type) {
+//        this.type = type;
+//    }
+// standard getters setters
+}
